12bbbbf3d812536da1ce3acd773334bbe1e0578c
Your Name <you@example.com>
2025-06-22 10:54:46 +0600

    backup

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestorage"
+    resource_group_name  = "tfstate-ovi-rg"
+    storage_account_name = "ovitfstatestorage"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
@@ -14,6 +14,7 @@ provider "azurerm" {
 variable "prefix" {
   default = "go-web-app-devops"
 }
+
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-rg"
   location = "East US"
