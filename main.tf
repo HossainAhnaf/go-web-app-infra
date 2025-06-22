16d76d468721eb12b780e4129820ab3004853b04
Your Name <you@example.com>
2025-06-22 10:29:16 +0600

    backup

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatestorage"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 }
